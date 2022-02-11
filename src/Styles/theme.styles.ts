b3e7c9ffe7e1bd33aad84fae91b41f53f995dc07
njsen <nicolas@jesenberger.fr>
2022-02-11 14:07:42 +0100

    Feat: Add color palette #8

--- src/Styles/theme.styles.ts
+++ src/Styles/theme.styles.ts
@@ -1,4 +1,38 @@
 const theme = {
+  color: {
+    primary: {
+      blue: `#305677`,
+      blueWhite: `#5F82A6`,
+      blueBlack: `#002D4B`,
+      orange: `#E68430`,
+      orangeWhite: `#FFB678`,
+      orangeBlack: `#CF7D38`,
+      paper: `#F7F3EA`,
+      white: `#E0DBD4`,
+    },
+    semantic: {
+      success: `#D4EDDA`,
+      successBorder: `#C3E6CB`,
+      successText: `#155724`,
+      info: `#CCE5FF`,
+      infoBorder: `#B8DAFF`,
+      infoText: `#0064B8`,
+      warning: `#FFF3CD`,
+      warningBorder: `#FFEEBA`,
+      warningText: `#896404`,
+      danger: `#F8D7DA`,
+      dangerBorder: `#F5C6CB`,
+      dangerText: `#9D1C24`,
+    },
+    neutral: {
+      black100: `#1F1F1F`,
+      black60: `##545454`,
+      black40: `#808080`,
+      black20: `#AEAEAE`,
+      black10: `#D6D6D6`,
+      black5: `#E8E8E8`,
+    },
+  },
   boxShadows: {
     panel: `box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.15)`,
     overlay: `0px 4px 12px rgba(0, 0, 0, 0.1)`,
