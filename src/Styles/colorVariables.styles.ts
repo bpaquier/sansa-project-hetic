9698bf1c73d6f618ce20050b16ccc6aba55a6c9a
Intisar <intisar.oulbani@gmail.com>
2022-03-09 15:50:23 +0100

    feat(style): Add all color variables not only colors alias to project

---
+++ src/Styles/colorVariables.styles.ts
@@ -0,0 +1,48 @@
+const colorVariables = {
+  //PRIMARY
+  primaryBlueColor: `#55789B`,
+  primaryBlueExtraLightColor: `#F0F7FF`,
+  primaryBlueLightColor: `#E3EAF2`,
+  primaryBlueDarkColor: `#305677`,
+  primaryBlueExtraDarkColor: `#002D4B`,
+  primaryOrangeColor: `#E68430`,
+  primaryOrangeLightColor: `#FFB678`,
+  primaryOrangeDarkColor: `#CF7D38`,
+  primaryPaperColor: `#F7F3EA`,
+  primaryWhiteColor: `#F7F3EA`,
+
+  //SEMANTIC
+  semanticSuccessBorderColor: ` #C3E6CB`,
+  semanticSuccessTextColor: `#155724`,
+  semanticInfoColor: `#CCE5FF`,
+  semanticInfoBorderColor: `#B8DAFF`,
+  semanticInfoTextColor: `#0064B8`,
+  semanticWarningColor: `#FFF3CD`,
+  semanticWarningBorderColor: `#FFEEBA`,
+  semanticWarningTextColor: `#896404`,
+  semanticDangerColor: `#F8D7DA`,
+  semanticDangerBorderColor: `#F5C6CB`,
+  primaryDangerTextColor: `#9D1C24`,
+
+  //NEUTRAL
+  neutralBlack100Color: `#1F1F1F`,
+  neutralBlack60Color: `#545454`,
+  neutralBlack40Color: `#808080`,
+  neutralBlack20Color: `#AEAEAE`,
+  neutralBlack10Color: `#D6D6D6`,
+  neutralBlack5Color: `#E8E8E8`,
+
+  // COLORS-ALIAS
+  // ICONS
+  iconsBlack40Color: `#808080`,
+
+  //BACKGROUND CTA
+  BackgroundCtaColor: `#112F45`,
+  BackgroundCtaDarkColor: `#305677`,
+  BackgroundCtaExtraDarkColor: `#0A2336`,
+
+  //BACKGROUND PAGE
+  backgroundPageColor: `#F7F3EA`
+};
+
+export default colorVariables;
