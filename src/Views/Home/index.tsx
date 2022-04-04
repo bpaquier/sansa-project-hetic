ac3a771209f5d7b2df017bf6fb41b5b5837c4267
bpaquier <bastien.paquier@gmail.com>
2022-04-04 14:42:36 +0200

    feat(Pages) delete unsused style

--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,12 +1,11 @@
-import Separator from "~/Components/Ui-kit/Separator";
 import Text from "~/Components/Ui-kit/Text";
 
 export default function Home(): JSX.Element {
   return (
     <>
-      <Text type="titleXL">Home</Text>
-      <Separator orientation="vertical" height="50px" />
-      <Text type="titleXL">hello</Text>
+      <Text type="titleXL" textAlign="center">
+        Home
+      </Text>
     </>
   );
 }
