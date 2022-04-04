9961bff0c7fb863e7ae34620caac4c16aa0fdbc4
bpaquier <bastien.paquier@gmail.com>
2022-04-04 14:12:22 +0200

    feat(Separator)  create component

--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,9 +1,12 @@
+import Separator from "~/Components/Ui-kit/Separator";
 import Text from "~/Components/Ui-kit/Text";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Text type="titleXL">Home</Text>
+      <Separator orientation="vertical" height="50px" />
+      <Text type="titleXL">hello</Text>
     </>
   );
 }
