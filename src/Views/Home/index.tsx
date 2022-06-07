9cfacb3e45115375ca7bfd5524ef1861dd50d8d0
bpaquier <bastien.paquier@gmail.com>
2022-06-07 11:35:55 +0200

    feat(Home) add link to login, just for test

--- src/Views/Home/index.tsx
+++ src/Views/Home/index.tsx
@@ -1,11 +1,16 @@
+import { useNavigate } from "react-router-native";
+
+import ButtonComponent from "~/Components/Ui-kit/Button";
 import Text from "~/Components/Ui-kit/Text";
 
 export default function Home(): JSX.Element {
+  const navigate = useNavigate();
   return (
     <>
       <Text type="titleXL" textAlign="center">
         Home
       </Text>
+      <ButtonComponent text="hello" onPress={() => navigate("/login")} />
     </>
   );
 }
