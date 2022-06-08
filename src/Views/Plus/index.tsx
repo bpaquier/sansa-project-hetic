73020e35b5795a10fa09a7e4fdcb93daa393bebc
Klochette <melina@arkhn.com>
2022-06-08 18:22:50 +0200

    fix(plus): mobile page when connected

--- src/Views/Plus/index.tsx
+++ src/Views/Plus/index.tsx
@@ -3,37 +3,51 @@ import {
   ButtonsContainerMobile,
   PageWrapper,
   PageWrapperBorne,
-  SansaDescriptionContainer,
-  AdministrativeHelpContainer,
   TitleWrapper,
-  PlusContainer,
-  ImageWrapper
+  PlusContainers,
+  ImageWrapper,
+  TextPlusWrapper
 } from "./styles";
 import PageContentWrapper from "~/Components/PageContentWrapper";
 import Button from "~/Components/Ui-kit/Button";
-import Separator from "~/Components/Ui-kit/Separator";
 import Text from "~/Components/Ui-kit/Text";
 import { useGlobalContext } from "~/Contexts/globalContext";
 
 export default function Plus(): JSX.Element {
   const { isMobile } = useGlobalContext();
 
-  const isConnected = false;
+  const isConnected = true;
 
   const GlobalWrapper = (
     isMobile ? PageWrapper : PageWrapperBorne
   ) as React.ElementType;
 
+  const plusContent = [
+    { title: "Nous contacter" },
+    { title: "Langues" },
+    { title: "Politique de confidentialité" },
+    { title: "Mentions légales" },
+    { title: "CGU" }
+  ];
+
+  const connectedContent = [
+    { title: "Mes notes" },
+    { title: "Coffre-fort numérique" },
+    { title: "Mes lieux favoris" }
+  ];
+
   return (
     <PageContentWrapper>
       <GlobalWrapper>
-        <TitleWrapper marginBottom={16}>
-          <Text type="titleL" textAlign="left" color="black20">
-            Plus
-          </Text>
-        </TitleWrapper>
+        {isMobile && (
+          <TitleWrapper marginBottom={16}>
+            <Text type="titleL" textAlign="left" color="black20">
+              Plus
+            </Text>
+          </TitleWrapper>
+        )}
         {!isConnected && (
-          <SansaDescriptionContainer>
+          <PlusContainers marginBottom paddingTop>
             <TitleWrapper marginBottom={8}>
               <Text type="titleL">SANSA</Text>
             </TitleWrapper>
@@ -45,9 +59,23 @@ export default function Plus(): JSX.Element {
               <Button text="Se connecter" fullWidth={isMobile} />
               <Button type="tertiary" text="S'inscrire" fullWidth={isMobile} />
             </ButtonsContainerMobile>
-          </SansaDescriptionContainer>
+          </PlusContainers>
         )}
-        <AdministrativeHelpContainer>
+        {isConnected && (
+          <PlusContainers marginBottom paddingTop>
+            <Text type="titleL">Bonjour Timothé !</Text>
+            {connectedContent.map((content) => {
+              return (
+                <TextPlusWrapper key={content.title}>
+                  <Text type="paragraph" color="grey">
+                    {content.title}
+                  </Text>
+                </TextPlusWrapper>
+              );
+            })}
+          </PlusContainers>
+        )}
+        <PlusContainers border marginBottom>
           <Text type="paragraph" color="black20">
             Consulter la liste
           </Text>
@@ -55,15 +83,19 @@ export default function Plus(): JSX.Element {
           <ImageWrapper>
             <SpotlightHelpIllustration />
           </ImageWrapper>
-        </AdministrativeHelpContainer>
-        <PlusContainer>
+        </PlusContainers>
+        <PlusContainers paddingTop>
           <Text type="titleL">Plus</Text>
-          <Text type="paragraph">Nous contacter</Text>
-          <Text type="paragraph">Langues</Text>
-          <Text type="paragraph">Politique de confidentialité</Text>
-          <Text type="paragraph">Mentions légales</Text>
-          <Text type="paragraph">CGU</Text>
-        </PlusContainer>
+          {plusContent.map((content) => {
+            return (
+              <TextPlusWrapper key={content.title}>
+                <Text type="paragraph" color="grey">
+                  {content.title}
+                </Text>
+              </TextPlusWrapper>
+            );
+          })}
+        </PlusContainers>
       </GlobalWrapper>
     </PageContentWrapper>
   );
