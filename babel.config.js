8fa2e492e2cf64b0413fc34800ea4163609af287
bpaquier <bastien.paquier@gmail.com>
2022-06-08 09:17:27 +0200

    feat(search) add map on search

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 module.exports = function (api) {
-  api.cache(true);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  api?.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
@@ -7,9 +10,9 @@ module.exports = function (api) {
         "babel-plugin-root-import",
         {
           rootPathPrefix: "~",
-          rootPathSuffix: "src",
-        },
-      ],
-    ],
+          rootPathSuffix: "src"
+        }
+      ]
+    ]
   };
 };
--- src/Views/Search/index.tsx
+++ src/Views/Search/index.tsx
@@ -1,11 +1,23 @@
-import Text from "~/Components/Ui-kit/Text";
+import { StyleSheet, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  }
+});
 
 export default function Search(): JSX.Element {
   return (
-    <>
-      <Text type="titleXL" textAlign="center">
-        Recherche
-      </Text>
-    </>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
   );
 }
