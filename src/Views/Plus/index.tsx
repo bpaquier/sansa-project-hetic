5bdec59b04cce6ff62587b3de4f0b6c6294039fe
Klochette <melina@arkhn.com>
2022-06-08 17:45:26 +0200

    fix(plus): add image and section help and plus

--- src/Views/Plus/index.tsx
+++ src/Views/Plus/index.tsx
@@ -1,12 +1,17 @@
+import SpotlightHelpIllustration from "./spotlightHelpIllustration";
 import {
   ButtonsContainerMobile,
   PageWrapper,
   PageWrapperBorne,
   SansaDescriptionContainer,
-  TitleWrapper
+  AdministrativeHelpContainer,
+  TitleWrapper,
+  PlusContainer,
+  ImageWrapper
 } from "./styles";
 import PageContentWrapper from "~/Components/PageContentWrapper";
 import Button from "~/Components/Ui-kit/Button";
+import Separator from "~/Components/Ui-kit/Separator";
 import Text from "~/Components/Ui-kit/Text";
 import { useGlobalContext } from "~/Contexts/globalContext";
 
@@ -42,6 +47,23 @@ export default function Plus(): JSX.Element {
             </ButtonsContainerMobile>
           </SansaDescriptionContainer>
         )}
+        <AdministrativeHelpContainer>
+          <Text type="paragraph" color="black20">
+            Consulter la liste
+          </Text>
+          <Text type="titleL">Aides administratives</Text>
+          <ImageWrapper>
+            <SpotlightHelpIllustration />
+          </ImageWrapper>
+        </AdministrativeHelpContainer>
+        <PlusContainer>
+          <Text type="titleL">Plus</Text>
+          <Text type="paragraph">Nous contacter</Text>
+          <Text type="paragraph">Langues</Text>
+          <Text type="paragraph">Politique de confidentialité</Text>
+          <Text type="paragraph">Mentions légales</Text>
+          <Text type="paragraph">CGU</Text>
+        </PlusContainer>
       </GlobalWrapper>
     </PageContentWrapper>
   );
--- src/Views/Plus/styles.ts
+++ src/Views/Plus/styles.ts
@@ -7,19 +7,15 @@ type TitleWrapperProps = {
   marginBottom: number;
 };
 
-const { boxShadow, color, shape } = Theme;
+const { boxShadow, color, shape, sizes } = Theme;
 
 export const PageWrapper = styled.View`
   width: 100%;
+  margin-bottom: ${sizes?.mobilePageMarginBottom}px;
 `;
 
 export const PageWrapperBorne = styled.View`
   width: 100%;
-  height: 100%;
-`;
-
-export const PageContent = styled.View`
-  flex-direction: column;
 `;
 
 export const TitleWrapper = styled.View<TitleWrapperProps>`
@@ -33,6 +29,26 @@ export const SansaDescriptionContainer = styled.View`
   box-shadow: ${Theme.boxShadow.panel};
   padding: ${getColumnWidth(1, true)};
   padding-top: 24px;
+  margin-bottom: 24px;
+`;
+
+export const AdministrativeHelpContainer = styled.View`
+  width: 100%;
+  border-radius: ${shape.radius.input}px;
+  background-color: ${color.primary.white};
+  box-shadow: ${Theme.boxShadow.panel};
+  border: 1px solid ${color.primary.blueDark};
+  margin-bottom: 24px;
+  padding: ${getColumnWidth(1, true)};
+`;
+
+export const PlusContainer = styled.View`
+  width: 100%;
+  border-radius: ${shape.radius.input}px;
+  background-color: ${color.primary.white};
+  box-shadow: ${Theme.boxShadow.panel};
+  padding: ${getColumnWidth(1, true)};
+  padding-top: 16px;
 `;
 
 export const SansaDescriptionTitleWrapper = styled.View`
@@ -45,4 +61,10 @@ export const SansaDescriptionTitleWrapper = styled.View`
 
 export const ButtonsContainerMobile = styled.View`
   margin-top: 40px;
+`;
+
+export const ImageWrapper = styled.View`
+  width: 100%;
+  height: 184px;
+  margin-top: 24px;
 `;
