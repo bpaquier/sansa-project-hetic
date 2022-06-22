55fd0643f5e1b7ec1758a95f6e08eba1225d8686
bpaquier <bastien.paquier@gmail.com>
2022-06-22 22:14:32 +0200

    feat(Accordion): create component

---
+++ src/Components/Accordion/index.tsx
@@ -0,0 +1,109 @@
+import React, {
+  ReactElement,
+  useEffect,
+  useState,
+  useRef,
+  useMemo
+} from "react";
+
+import { Animated, Easing, View } from "react-native";
+
+import { AccordionWrapper, Head, TextWrapper } from "./styles";
+import ArrowDown from "~/Components/Icons/System/Arrows/ArrowDown";
+import ArrowUp from "~/Components/Icons/System/Arrows/ArrowUp";
+import Text from "~/Components/Ui-kit/Text";
+
+export interface AccordionProps {
+  forceOpen?: boolean;
+  headText: string;
+  content: ReactElement;
+  initialState?: "open" | "closed";
+}
+
+export default function Accordion({
+  forceOpen,
+  headText,
+  content,
+  initialState = "closed"
+}: AccordionProps): JSX.Element {
+  const [isOpen, setIsOpen] = useState<boolean>(
+    initialState === "open" ? true : false
+  );
+  const [contentHeight, setContentHeight] = useState<number>();
+  const heightAnimationValueHolder = useRef(
+    new Animated.Value(initialState === "open" ? 1 : 0)
+  );
+  const rotateAnimationValueHolder = useRef(
+    new Animated.Value(initialState === "open" ? 1 : 0)
+  );
+
+  useEffect(() => {
+    Animated.timing(heightAnimationValueHolder?.current, {
+      toValue: isOpen ? 1 : 0,
+      duration: 300,
+      useNativeDriver: false,
+      easing: Easing?.circle
+    }).start();
+    Animated.timing(rotateAnimationValueHolder?.current, {
+      toValue: isOpen ? 1 : 0,
+      duration: 400,
+      useNativeDriver: false,
+      easing: Easing?.linear
+    }).start();
+  }, [isOpen]);
+
+  useEffect(() => {
+    setIsOpen(forceOpen);
+  }, [forceOpen]);
+
+  const heightAnimation = useMemo(
+    () =>
+      heightAnimationValueHolder.current.interpolate({
+        inputRange: [0, 1],
+        outputRange:
+          initialState === "open" ? [contentHeight, 0] : [0, contentHeight]
+      }),
+    [contentHeight, initialState]
+  );
+
+  const rotateAnimation = heightAnimationValueHolder.current.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "180deg"]
+  });
+
+  return (
+    <AccordionWrapper>
+      <Head onPress={() => setIsOpen((prev) => !prev)} activeOpacity={0.7}>
+        <TextWrapper>
+          <Text>{headText}</Text>
+        </TextWrapper>
+        <Animated.View style={[{ transform: [{ rotate: rotateAnimation }] }]}>
+          {initialState === "open" ? <ArrowUp /> : <ArrowDown />}
+        </Animated.View>
+      </Head>
+      <Animated.View
+        style={[
+          {
+            height: !isNaN(contentHeight)
+              ? heightAnimation
+              : initialState === "open"
+              ? "auto"
+              : 1,
+            ...(initialState === "closed" && {
+              opacity: !isNaN(contentHeight) ? 1 : 0
+            })
+          }
+        ]}
+      >
+        <View
+          onLayout={(e) => {
+            const { height } = e.nativeEvent.layout;
+            !contentHeight && setContentHeight(height);
+          }}
+        >
+          {content}
+        </View>
+      </Animated.View>
+    </AccordionWrapper>
+  );
+}
---
+++ src/Components/Accordion/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+import theme from "~/Styles/theme.styles";
+const { color, shape } = theme;
+
+export const AccordionWrapper = styled.View`
+  width: 100%;
+  border-radius: ${shape?.radius?.input}px;
+  background-color: ${color?.primary?.white};
+`;
+
+export const Head = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px 32px;
+`;
+
+export const TextWrapper = styled.View`
+  flex: 1 1;
+`;
