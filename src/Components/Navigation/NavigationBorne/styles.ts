45fcfa8aaf34d8c00fecab26edc2ecdd6a925ebe
Nicolas Jesenberger <71784091+njsen@users.noreply.github.com>
2022-06-28 13:43:14 +0200

    Update src/Components/Navigation/NavigationBorne/styles.ts
    Co-authored-by: Melina <47388675+Klochette@users.noreply.github.com>

--- src/Components/Navigation/NavigationBorne/styles.ts
+++ src/Components/Navigation/NavigationBorne/styles.ts
@@ -38,7 +38,10 @@ export const NavButtonContainer = styled.View<NavButtonContainerProps>`
   ${({ active }: any) =>
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     active &&
-    `
+  ${({ active }) =>
+      active && `border-color: ${Theme.color.primary.blueExtraDark};`};
+  
+  ${({ spaceTop }) => spaceTop && `margin-top: 28px;`};
     border-color: ${Theme.color.primary.blueExtraDark};
   `};
 
