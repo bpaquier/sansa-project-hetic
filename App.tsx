0a868a85902e6f6cf8944d15a5b0b9404f048506
bpaquier <bastien.paquier@gmail.com>
2022-07-06 15:52:04 +0200

    feat(All): lock orientation according to windows width

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+
+import * as ScreenOrientation from "expo-screen-orientation";
+import { Dimensions } from "react-native";
 import { NativeRouter, Routes, Route } from "react-router-native";
 
 import LanguagesMenu from "~/Components/LanguagesMenu";
 import GlobalProvider from "~/Contexts/globalContext";
+import theme from "~/Styles/theme.styles";
 import Login from "~/Views/Account/Login";
 import Register from "~/Views/Account/Register";
 import Home from "~/Views/Home";
@@ -11,6 +16,14 @@ import Search from "~/Views/Search";
 import "~/locales/i18n";
 
 export default function App() {
+  useEffect(() => {
+    if (Dimensions?.get("window")?.width > theme?.sizes?.breakPoint) {
+      ScreenOrientation?.lockAsync(5);
+    } else {
+      ScreenOrientation?.lockAsync(3);
+    }
+  }, []);
+
   return (
     <NativeRouter>
       <GlobalProvider>
