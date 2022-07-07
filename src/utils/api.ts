f0b5eb9a31597e1760a2884c209de89c618b65cd
bpaquier <bastien.paquier@gmail.com>
2022-07-07 16:48:51 +0200

    refactor(utils): delete qs serializer, add const fot api end point

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import axios from "axios";
 
+const API_ENDPOINT = "http://sansah.vhswebs.com/api";
+
 export async function getOrgaByServices(services: string[]) {
   return await axios
-    .get(
-      `http://sansah.vhswebs.com/api/getallorgaByService/${services?.join("~")}`
-    )
+    .get(`${API_ENDPOINT}/getallorgaByService/${services?.join("~")}`)
     ?.then((resp) => resp)
     ?.catch((error) => error);
 }
 
 export async function getOrgaById(id: number) {
   return await axios
-    .get(`http://sansah.vhswebs.com/api/getorgabyid/${id}`)
+    .get(`${API_ENDPOINT}/getorgabyid/${id}`)
     ?.then((resp) => resp)
     ?.catch((error) => error);
 }
