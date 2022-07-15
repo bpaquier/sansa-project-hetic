fc0bcdfdbfe40f0582b4134abe8dd5b5376a9969
bpaquier <bastien.paquier@gmail.com>
2022-07-15 10:37:25 +0200

    feat(useApi): clean axios call in useEffect return

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,7 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
+import { useEffect, useRef } from "react";
+
 import axios from "axios";
+
 export default function useApi() {
   const API_ENDPOINT = "https://sansah.vhswebs.com/api";
+  const unmounted = useRef<boolean>(false);
+
+  useEffect(() => {
+    const source = axios?.CancelToken?.source();
+
+    return () => {
+      unmounted.current = true;
+      source.cancel("clean up  cancelling");
+    };
+  }, []);
 
   const getOrgaByServices = async (services: string[]) => {
     return await axios
